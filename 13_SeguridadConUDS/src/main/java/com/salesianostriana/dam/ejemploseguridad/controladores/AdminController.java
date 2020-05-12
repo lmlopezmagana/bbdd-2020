3ce7e750a5ad5f59f3949c097160c86e9e75212a
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2020-05-12 11:42:38 +0200

    Se incluye un ejemplo de cómo rescatar al usuario logueado en un controlador.

--- 13_SeguridadConUDS/src/main/java/com/salesianostriana/dam/ejemploseguridad/controladores/AdminController.java
+++ 13_SeguridadConUDS/src/main/java/com/salesianostriana/dam/ejemploseguridad/controladores/AdminController.java
@@ -1,8 +1,12 @@
 package com.salesianostriana.dam.ejemploseguridad.controladores;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.salesianostriana.dam.ejemploseguridad.seguridad.modelo.Usuario;
 
 @Controller
 @RequestMapping("/admin/")
@@ -11,6 +15,12 @@ public class AdminController {
 	@GetMapping("/")
 	public String index() {
 		return "administrador/index";
+	}
+	
+	@GetMapping("/info")
+	public String info(@AuthenticationPrincipal Usuario usuario, Model model) {
+		model.addAttribute("nombre", usuario.getNombre() + " " + usuario.getApellidos());
+		return "administrador/info";
 	}
 
 }
